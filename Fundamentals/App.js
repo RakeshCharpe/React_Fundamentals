c282b073e0be46ff72abbfaad773c3d82ec1bbda
RakeshCharpe <rakeshcharpe99@gmail.com>
2024-04-27 01:30:57 +0530

    added info about elements

--- Fundamentals/App.js
+++ Fundamentals/App.js
@@ -1,3 +1,7 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React !!!");
+const heading = React.createElement("h1", { id: "heading" }, "Hello World from React !!!");
+//React element is nothing but the plain javascript object heading returns the object 
+console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// render function is responsible for the take the js object , create Tag and put it in DOM
 root.render(heading);
