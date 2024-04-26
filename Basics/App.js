e5242440a0763273c8d405cb859ee67b25826d95
RakeshCharpe <rakeshcharpe99@gmail.com>
2024-04-27 04:51:18 +0530

    modified the files

--- Basics/App.js
+++ Basics/App.js
@@ -1,15 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
 //Creating the nested element
 //const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", {}, "I'm h1 tag")));
 
 // if we wanted to create siblings then we need to used array 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "I'm parent tag"),
+    React.createElement("h2", {}, "I'm child tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "I'm parent tag"),
+    React.createElement("h2", {}, "I'm child tag"),
   ]),
 ]);
 // this create complex code for that we used JSX to make life easy but this is the core react code
@@ -22,3 +25,5 @@ console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // render function is responsible for the take the js object , create Tag and put it in DOM
 root.render(parent);
+
+
